7386b284534b7bb1bae434609bb667d310464732
mohit-devrev <mohit.kumar@devrev.ai>
2025-07-18 12:19:15 +0530

    test-pr-7

--- infrastructure/terraform/modules/pr-services/main.tf
+++ infrastructure/terraform/modules/pr-services/main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "shared" {
   }
 }
 
-# Service Discovery Namespace for this PR (used by Service Connect)
+# Service Discovery Namespace  for this PR (used by Service Connect)
 resource "aws_service_discovery_private_dns_namespace" "pr" {
   name = "pr-${var.pr_number}.local"
   vpc  = var.vpc_id
